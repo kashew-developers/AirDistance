885b08b5497b50dc1750f1fee3ec36166d67f511
kashew-developers <kashewdevelopers@gmail.com>
2021-01-08 22:50:32 +0530

    Create AirDistance View

---
+++ app/src/main/java/in/kashewdevelopers/airdistance/AirDistance.kt
@@ -0,0 +1,11 @@
+package `in`.kashewdevelopers.airdistance
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AirDistance : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_air_distance)
+    }
+}
