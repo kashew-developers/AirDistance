2e8e1b2bfce656725366a1f956fd075878a4037d
kashew-developers <kashewdevelopers@gmail.com>
2021-01-09 23:56:16 +0530

    translate LocationObject to kotlin

---
+++ app/src/main/java/in/kashewdevelopers/airdistance/data_containers/LocationObject.kt
@@ -0,0 +1,5 @@
+package `in`.kashewdevelopers.airdistance.data_containers
+
+data class LocationObject(val placeName: String,
+                          val latitude: Double,
+                          val longitude: Double)
